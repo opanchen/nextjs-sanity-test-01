94c17bee2e6148906723042a2a7cbd4567904921
Oleh Panchenko <106483317+opanchen@users.noreply.github.com>
2023-12-16 15:15:06 +0100

    init

---
+++ sanity.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+
+import schemas from "@/sanity/schemas";
+
+const config = defineConfig({
+  projectId: "jfmbqv1m", //env
+  dataset: "production", //env
+  apiVersion: "2023-12-16", //env
+
+  title: "Next Sanity Test-01",
+
+  basePath: "/admin",
+
+  plugins: [deskTool()],
+
+  schema: { types: schemas },
+});
+
+export default config;
---
+++ src/sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: process.env.STUDIO_PROJECT_ID as string,
+  dataset: process.env.STUDIO_DATASET as string,
+  apiVersion: process.env.STUDIO_API_VERSION as string,
+};
+
+export default config;
